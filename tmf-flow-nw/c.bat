e9dd202fdb0afd4809b431fbe24b21fc3970d7ed
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-14 07:13:16 +0530

    Updated flow configuration

---
+++ tmf-flow-nw/c.bat
@@ -0,0 +1,4 @@
+docker-compose -f rconsumer.yaml stop
+docker-compose -f rconsumer.yaml rm -f
+#docker-compose -f rconsumer.yaml pull   
+docker-compose -f rconsumer.yaml up
---
+++ tmf-flow-nw/p.bat
@@ -0,0 +1,4 @@
+docker-compose -f rproducer.yaml stop
+docker-compose -f rproducer.yaml rm -f
+#docker-compose -f rproducer.yaml pull   
+docker-compose -f rproducer.yaml up
